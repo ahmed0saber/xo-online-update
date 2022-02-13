f38fc1804b7ab74bfe322b1bec2a19f3acca7151
Ahmed Saber <ahmed0saber33@gmail.com>
2022-02-14 01:23:58 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /*  LOADING  */
-const myTimeout = setTimeout(loading, 2000);
+const myTimeout = setTimeout(loading, 1000);
 function loading(){
     document.getElementsByClassName("loader")[0].style.display = "none";
     document.getElementsByTagName("main")[0].style.display = "flex";
