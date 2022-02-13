14e415efb1d000e5ec142acb53889c82339dc84b
Ahmed Saber <ahmed0saber33@gmail.com>
2022-02-13 20:33:54 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-/*  EMPTY  */
+/*  LOADING  */
+const myTimeout = setTimeout(loading, 2000);
+function loading(){
+    document.getElementsByClassName("loader")[0].style.display = "none";
+    document.getElementsByTagName("main")[0].style.display = "flex";
+    document.getElementsByTagName("header")[0].style.display = "flex";
+}
