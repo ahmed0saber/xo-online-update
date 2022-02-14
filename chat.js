73381bdf7ab89504f36906d7bbacb08118c4a8d0
Ahmed Saber <ahmed0saber33@gmail.com>
2022-02-14 21:20:24 +0200

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,5 @@
+var screen = document.getElementsByClassName("fullscreen")[0];
+screen.style.minHeight = (window.innerHeight - 100);
+screen.style.maxHeight = (window.innerHeight - 100);
+screen.style.height = (window.innerHeight - 100);
+console.log((window.innerHeight - 100))
